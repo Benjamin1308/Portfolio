d8c32800e7575293d6c40abd086900d926f50ef5
nxvinh2197 <nxvinh@apcs.vn>
2018-03-21 00:17:30 +0700

    Contact form

--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
 		$(this).toggleClass('active');
 		$('.nav-bar').toggleClass('opened');
 	});
+  $('.input-item').on('focus', function(){
+  		$(this).parent().addClass("form-item-focus")
+  });
+  $('.input-item').on('blur', function(){
+  		$(this).parent().removeClass("form-item-focus")
+  });
+ $("body").on('input propertychange', '.form-item', function(e) {
+  		$(this).toggleClass("form-item-filled",!! $(e.target).val());;
+  });
+
 });
