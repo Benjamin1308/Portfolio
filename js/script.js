0a6d517dea41e329ca4b220b098a0f5e66555306
nxvinh2197 <nxvinh@apcs.vn>
2018-03-25 17:50:04 +0700

    Finished footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-  $("#active").focus();
+  $(".nav-item").on('focus', function(){
+  		$(this).focus();
+  });
   $('.toggle').on('click', function(){
 		$(this).toggleClass('active');
 		$('.nav-bar').toggleClass('opened');
@@ -13,5 +15,6 @@ $(document).ready(function(){
  $("body").on('input propertychange', '.form-item', function(e) {
   		$(this).toggleClass("form-item-filled",!! $(e.target).val());;
   });
-
+ $('body').scrollspy({target: '.nav-bar'})
+ 
 });
